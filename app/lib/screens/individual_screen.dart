45c8b7434370180a88e2ea26dcda7626f33c32f1
Rihem Zairi Gate <rihem.zairi@gate-training.com>
2023-11-13 11:06:32 +0100

    create widget emoji

--- app/lib/screens/individual_screen.dart
+++ app/lib/screens/individual_screen.dart
@@ -1,6 +1,8 @@
 import 'package:app/model/chat_model.dart';
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter/foundation.dart' as foundation;
 
 class IndividualScreen extends StatefulWidget {
   const IndividualScreen({Key? key, this.chatModel}) : super(key: key);
@@ -10,6 +12,23 @@ class IndividualScreen extends StatefulWidget {
 }
 
 class _IndividualScreenState extends State<IndividualScreen> {
+  bool show = false;
+  FocusNode focusNode = FocusNode();
+  TextEditingController controller = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    focusNode.addListener(() {
+      if (focusNode.hasFocus) {
+        setState(() {
+          show = false;
+        });
+      } else {
+        print("No focus........");
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -94,63 +113,156 @@ class _IndividualScreenState extends State<IndividualScreen> {
       body: Container(
         height: MediaQuery.sizeOf(context).height,
         width: MediaQuery.sizeOf(context).width,
-        child: Stack(children: [
-          ListView(),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Row(
-              children: [
-                Container(
-                  width: MediaQuery.sizeOf(context).width - 60,
-                  child: Card(
-                    margin: EdgeInsets.only(left: 2, right: 2, bottom: 8),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(25)),
-                    child: TextFormField(
-                      textAlignVertical: TextAlignVertical.center,
-                      decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "Type a message ",
-                          contentPadding: EdgeInsets.all(5),
-                          prefixIcon: IconButton(
-                            icon: Icon(Icons.emoji_emotions),
+        child: WillPopScope(
+          child: Stack(children: [
+            ListView(),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        width: MediaQuery.sizeOf(context).width - 60,
+                        child: Card(
+                          margin: EdgeInsets.only(left: 2, right: 2, bottom: 8),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25)),
+                          child: TextFormField(
+                            controller: controller,
+                            focusNode: focusNode,
+                            textAlignVertical: TextAlignVertical.center,
+                            decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "Type a message ",
+                                contentPadding: EdgeInsets.all(5),
+                                prefixIcon: IconButton(
+                                  icon: Icon(Icons.emoji_emotions),
+                                  onPressed: () {
+                                    //! focusNode.unfocus()
+                                    //Cette méthode supprime le focus d'un nœud qui a le focus principal, annule toutes les demandes en attente pour le focus, tout en définissant le focus principal sur un autre nœud en fonction de la disposition.
+                                    focusNode.unfocus();
+                                    setState(() {
+                                      show = !show;
+                                    });
+                                  },
+                                ),
+                                suffixIcon: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    IconButton(
+                                      onPressed: () {
+                                        showModalBottomSheet(
+                                            backgroundColor: Colors.transparent,
+                                            context: context,
+                                            builder: (builder) =>
+                                                bottomSheet());
+                                      },
+                                      icon: Icon(Icons.attach_file),
+                                    ),
+                                    IconButton(
+                                      onPressed: () {},
+                                      icon: Icon(Icons.camera_alt),
+                                    )
+                                  ],
+                                )),
+                            keyboardType: TextInputType.multiline,
+                            maxLines: 5,
+                            minLines: 1,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding:
+                            const EdgeInsets.only(bottom: 8, right: 5, left: 2),
+                        child: CircleAvatar(
+                          radius: 25,
+                          backgroundColor: Color(0xFF128C7E),
+                          child: IconButton(
+                            icon: Icon(Icons.mic, color: Colors.white),
                             onPressed: () {},
                           ),
-                          suffixIcon: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              IconButton(
-                                onPressed: () {},
-                                icon: Icon(Icons.attach_file),
-                              ),
-                              IconButton(
-                                onPressed: () {},
-                                icon: Icon(Icons.camera_alt),
-                              )
-                            ],
-                          )),
-                      keyboardType: TextInputType.multiline,
-                      maxLines: 5,
-                      minLines: 1,
-                    ),
+                        ),
+                      )
+                    ],
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 8, right: 5, left: 2),
-                  child: CircleAvatar(
-                    radius: 25,
-                    backgroundColor: Color(0xFF128C7E),
-                    child: IconButton(
-                      icon: Icon(Icons.mic, color: Colors.white),
-                      onPressed: () {},
-                    ),
-                  ),
-                )
-              ],
-            ),
-          )
-        ]),
+                  show ? emojiSelect() : Container()
+                ],
+              ),
+            )
+          ]),
+          //Si le rappel renvoie un Future qui se résout à false,
+          //la route englobante ne sera pas affichée.
+          onWillPop: () {
+            if (show) {
+              setState(() {
+                show = false;
+              });
+            } else {
+              Navigator.pop(context);
+            }
+            return Future.value(false);
+          },
+        ),
       ),
     );
+  }
+
+  Widget bottomSheet() {
+    return Container(
+      height: 278,
+      width: MediaQuery.sizeOf(context).width,
+      child: Card(
+        margin: EdgeInsets.all(18),
+      ),
+    );
+  }
+
+  Widget emojiSelect() {
+    return SizedBox(
+        height: 250,
+        child: EmojiPicker(
+          onEmojiSelected: (category, emoji) {
+            setState(() {
+              controller.text = controller.text + emoji.emoji;
+            });
+          },
+          // textEditingController: _controller,
+          // onBackspacePressed: _onBackspacePressed,
+          config: Config(
+            columns: 7,
+            // Issue: https://github.com/flutter/flutter/issues/28894
+            emojiSizeMax: 32 *
+                (foundation.defaultTargetPlatform == TargetPlatform.iOS
+                    ? 1.30
+                    : 1.0),
+            verticalSpacing: 0,
+            horizontalSpacing: 0,
+            gridPadding: EdgeInsets.zero,
+            initCategory: Category.RECENT,
+            bgColor: const Color(0xFFF2F2F2),
+            indicatorColor: Colors.blue,
+            iconColor: Colors.grey,
+            iconColorSelected: Colors.blue,
+            backspaceColor: Colors.blue,
+            skinToneDialogBgColor: Colors.white,
+            skinToneIndicatorColor: Colors.grey,
+            enableSkinTones: true,
+            recentTabBehavior: RecentTabBehavior.RECENT,
+            recentsLimit: 28,
+            replaceEmojiOnLimitExceed: false,
+            noRecents: const Text(
+              'No Recents',
+              style: TextStyle(fontSize: 20, color: Colors.black26),
+              textAlign: TextAlign.center,
+            ),
+            loadingIndicator: const SizedBox.shrink(),
+            tabIndicatorAnimDuration: kTabScrollDuration,
+            categoryIcons: const CategoryIcons(),
+            buttonMode: ButtonMode.MATERIAL,
+            checkPlatformCompatibility: true,
+          ),
+        ));
   }
 }
